1bac5de0077329913aceacdf0cd3fd6d2e676580
arschedev <98528463+arschedev@users.noreply.github.com>
2023-04-10 22:30:51 +0300

    added scripts & updated
    version "1.0.1"

---
+++ scripts/install.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/bash
+echo -e "*********************************\n arschedev's pathedit install.sh \n*********************************\n"
+
+# Dependencies
+sudo echo "* Dependencies: "
+
+# Perl
+echo -n "-- Perl: "
+perl -v &>/dev/null || {
+    echo -e "not installed\n\nInstall Perl (https://www.perl.org/get.html)\n..."
+    exit 1
+}
+
+echo -e "installed\n"
+
+# Download
+echo -n "* Download... "
+curl https://raw.githubusercontent.com/arschedev/pathedit/main/src/pathedit --output pathedit --silent || {
+    echo -e "\nInstaller: unexpected error occurred while download\n..."
+    exit 1
+}
+
+printf " %.0s" {1..14}
+echo -e "OK\n"
+
+# Installation
+echo "* Install: "
+
+{
+    echo -n "-- Moving to /usr/bin... " &&
+    sudo mv ./pathedit /usr/bin/pathedit &&
+    printf " %.0s" {1..3} &&
+    echo "OK" &&
+    echo -n "-- Marking as executable... " &&
+    sudo chmod +x /usr/bin/pathedit &&
+    echo "OK"
+} || {
+    echo -e "\nInstaller: unexpected error occurred while install\n..."
+    exit 1
+}
+
+echo -e -n "\nSuccessfully installed "
+pathedit -v | grep version
+echo ":D"
---
+++ scripts/uninstall.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+echo -e "***********************************\n arschedev's pathedit uninstall.sh \n***********************************\n"
+
+{
+    sudo echo -n "chmod -x /usr/bin/pathedit... " &&
+    sudo chmod -x /usr/bin/pathedit &&
+    echo "OK"
+    echo -n "rm /usr/bin/pathedit... " &&
+    sudo rm /usr/bin/pathedit &&
+    printf " %.0s" {1..6} &&
+    echo "OK"
+} || {
+    echo -e "\nInstaller: unexpected error occurred while uninstall\n..."
+    exit 1
+}
+
+echo -e "\nSuccessfully uninstalled\n..."
